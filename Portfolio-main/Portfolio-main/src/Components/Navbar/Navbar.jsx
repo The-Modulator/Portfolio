3d5a612f857ac553abc4d575f681b4a9e9bacff9
Elias Gabriel <elias_ghabriel@hotmail.com>
2024-04-11 15:15:31 +0100

    nice planets added

--- Portfolio-main/Portfolio-main/src/Components/Navbar/Navbar.jsx
+++ Portfolio-main/Portfolio-main/src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,17 @@ import {motion} from "framer-motion"
 import Sidebar from "./sidebar/Sidebar";
 
 const Navbar = () => {
+
+    const openNewTab = () => {
+        // Specify the URL you want to open in the new tab
+        const url = 'https://github.com/the-modulator';
+    
+        // Open a new tab with the specified URL
+        window.open(url, '_blank');
+      };
+
+      
+
 return (
     <div className="navbar">
         {/*Sidebar*/}
@@ -10,9 +21,10 @@ return (
         <div className="wrapper">
             <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.3}}>The-Modulator</motion.span>
             <div className="social>">
-                <a href='#'><img src="/instagram.png" alt="" /></a>
-                <a href='#'><img src="/youtube.png" alt="" /></a>
-                <a href='#'><img src="/github.png" alt="" /></a>
+                <a href="https://github.com/the-modulator" target="_blank" rel="noopener noreferrer"><img src="/instagram.png" alt=""  /></a>
+                <a href='https://github.com/the-modulator' target="_blank" rel="noopener noreferrer"><img src="/youtube.png" alt="" /></a>
+                <a href='https://github.com/the-modulator' target="_blank" rel="noopener noreferrer"><img src="/github.png" alt="" /></a>
+
 
             </div>
         </div>
